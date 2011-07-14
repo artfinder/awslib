6231af06e364070efad6760a97543984914fe939
James Aylett <james@tartarus.org>
2011-07-14 16:34:46 +0000

    v0.3: simple commands for flipping instances through an ELB note that this is a very dumb way of doing things, because ELB is pretty dumb itself (there's an 'instance registration' period, so what we should actually do is block and query the new instance states before deregistering the old ones).

--- awslib/ec2.py
+++ awslib/ec2.py
@@ -5,6 +5,25 @@ import boto.ec2
 
 ### EC2 HOSTLIST QUERIES
 
+def instances_from_security_group(
+    group_name,
+    region=None,
+    aws_access_key=None,
+    aws_secret_key=None
+):
+    if aws_access_key is None:
+        aws_access_key = os.environ['AWS_ACCESS_KEY']
+    if aws_secret_key is None:
+        aws_secret_key = os.environ['AWS_SECRET_KEY']
+    
+    if region:
+        ec2c = boto.ec2.connect_to_region(region, aws_access_key_id=env.access_key_id, aws_secret_access_key=env.secret_access_key)
+    else:
+        ec2c = boto.ec2.connection.EC2Connection(aws_access_key, aws_secret_key)
+    sg   = ec2c.get_all_security_groups( groupnames=[group_name] )[0]
+    return sg.instances()
+
+
 def hosts_from_security_group(
     group_name,
     display=False,
@@ -13,18 +32,13 @@ def hosts_from_security_group(
     aws_secret_key=None,
     return_private_dns_names=False,
 ):
-    if aws_access_key is None:
-        aws_access_key = os.environ['AWS_ACCESS_KEY']
-    if aws_secret_key is None:
-        aws_secret_key = os.environ['AWS_SECRET_KEY']
     sg_hosts = []
-    
-    if region:
-        ec2c = boto.ec2.connect_to_region(region, aws_access_key_id=env.access_key_id, aws_secret_access_key=env.secret_access_key)
-    else:
-        ec2c = boto.ec2.connection.EC2Connection(aws_access_key, aws_secret_key)
-    sg   = ec2c.get_all_security_groups( groupnames=[group_name] )[0]
-    for i in sg.instances():
+    for i in instances_from_security_group(
+        group_name,
+        region,
+        aws_access_key,
+        aws_secret_key
+    ):
         if i.state!='running':
             if display:
                 print "Skipping", i.private_dns_name, "(not running)"
@@ -36,6 +50,35 @@ def hosts_from_security_group(
         if display:
             print i.private_dns_name
     return sg_hosts
+
+
+def flip_elb_to(elb_name, instance_ids):
+    """
+    Flip the ELB to point to the new instances, returning the list of
+    its previous instance ids.
+    """
+    
+    aws_access_key = os.environ['AWS_ACCESS_KEY']
+    aws_secret_key = os.environ['AWS_SECRET_KEY']
+    elbc = boto.ec2.elb.ELBConnection(aws_access_key, aws_secret_key)
+    bh = elbc.get_all_load_balancers(elb_name)[0]
+    old_instance_ids = map(lambda x: x.id, bh.instances)
+    bh.register_instances(instance_ids)
+    bh.deregister_instances(old_instance_ids)
+    return old_instance_ids
+
+
+def flip_elb_to_security_group(elb_name, security_group):
+    """
+    Flip the ELB to all instances in a given security group.
+    Return the instance ids previously registered with the ELB.
+    """
+
+    instances = hosts_from_security_group(security_group)
+    return flip_elb_to(
+        elb_name,
+        [i.id for i in instances if i.state=='running']
+    )
 
 
 def hosts_from_elb(elb_name):
